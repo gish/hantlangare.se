7e12785d1ae84bfbc8db190f001d9e06017393fa
Erik Hedberg <erik@hedberg.at>
2023-08-16 16:38:40 +0200

    ci: adapt deploy to project

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ set -e
 
 rm -rf dist
 npm run build
-cp src/*.png dist/
 terraform apply --auto-approve
-aws s3 sync dist s3://designkod.se/ --exclude "*.map"
-aws cloudfront create-invalidation --distribution-id E2AE8XCF1HCG31 --paths "/*" --no-cli-pager
+aws s3 rm s3://hantlangare.se/ --recursive
+aws s3 sync dist s3://hantlangare.se/ --exclude "*.map"
+CLOUDFRONTID=`aws cloudfront list-distributions --query "DistributionList.Items[?Origins.Items[?Id=='S3-hantlangare.se']].{ID:Id, Origin:Origins.Items[].DomainName}" --no-cli-pager | jq -r '.[0].ID'`
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONTID --paths "/*" --no-cli-pager
