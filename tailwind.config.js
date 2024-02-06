2d35005367d69d4ea2c31c77889f79660db00558
Erik Hedberg <erik@hedberg.at>
2024-02-06 08:52:27 +0100

    update link color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,19 @@ module.exports = {
     extend: {
       colors: {
         primary: colors.stone[100],
-        secondary:"#558B6E",
+        secondary: {
+          50: "#ECF3EF",
+          100: "#DCEAE2",
+          200: "#B9D4C6",
+          300: "#93BDA7",
+          400: "#71A88A",
+          500: "#558B6E",
+          600: "#446F58",
+          700: "#325241",
+          800: "#23392D",
+          900: "#111C17",
+          950: "#080D0A",
+        },
         accent: "#704C5E",
       },
     },
